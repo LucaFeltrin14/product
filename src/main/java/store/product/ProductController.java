47459af4213792ae0cb9ddc283d4aaf975760a25
LucaFeltrin14 <lucasf6@al.insper.edu.br>
2025-05-23 14:28:15 -0300

    product from platfp

---
+++ src/main/java/store/product/ProductController.java
@@ -0,0 +1,22 @@
+package store.product;
+
+import java.util.List;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@FeignClient(name = "product", url = "http://product:8080")
+public interface ProductController {
+
+    @PostMapping("/product")
+    ResponseEntity<ProductOut> create(@RequestBody ProductIn productIn);
+
+    @GetMapping("/product")
+    ResponseEntity<List<ProductOut>> findAll();
+
+    @GetMapping("/product/{id}")
+    ResponseEntity<ProductOut> findById(@PathVariable String id);
+
+    @DeleteMapping("/product/{id}")
+    ResponseEntity<Void> delete(@PathVariable String id);
+}
